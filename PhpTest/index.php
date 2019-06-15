2795495498ce0eca23bcaa9aa23ed4e4157481c8
shim sunghyun <sim51177@hotmail.com>
2019-06-16 05:50:05 +0900

    php 테스트 추가

--- PhpTest/index.php
+++ PhpTest/index.php
@@ -1,16 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
-
 <body>
-    <a href="HtmlCalc/index.html">HtmlCalc</a><br />
-    <a href="PhpTest/index.php">PhpTest</a>
+<?php
+  echo "hello world\n";
+?>
 </body>
-
 </html>
